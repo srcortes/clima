c30b308ea728c9fd7b500a5936a37ddbdbeb784e
= <=>
2020-09-17 11:11:37 -0500

    Is create file component.js and add components of materialize css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {Fragment} from 'react';
-import Header from './Components/Header'
-
+import React, { Fragment } from "react";
+import Header from "./Components/Header";
+import Formulario from "./Components/Formulario"
 
 function App() {
   return (
     <Fragment>
-       <Header
-          titulo='Weather Clima App'
-       />
+      <Header titulo="Weather Clima App" />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario/>
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
---
+++ src/Components/Formulario.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Formulario = () => {
+  return (
+    <form>
+      <div className="input-field col s12">
+        <input type="text" name="ciudad" id="ciudad" />
+        <label htmlFor="ciudad">Ciudad:</label>
+      </div>
+
+      <div className="input-field col s12">
+        <select name="pais" id="pais">
+            <option value="">--Seleccione un Pais</option>
+        </select>
+        <label htmlFor="pais">Pais:</label>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
