e4a7102c7bd491a03063c76817c5b105be99c54a
= <=>
2020-09-18 08:28:21 -0500

    It is created, function for read input and useState for save information

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Formulario = () => {
+  //state del formulario
+  const [busqueda, guardarBusqueda] = useState({
+    ciudad: "",
+    pais: ""
+  });
+
+  //Extraer ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  //funcion que coloca los elementos en el state
+  const handleChange = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="input-field col s12">
-        <input type="text" name="ciudad" id="ciudad" />
+        <input
+          type="text"
+          name="ciudad"
+          id="ciudad"
+          value={ciudad}
+          onChange={handleChange}
+        />
         <label htmlFor="ciudad">Ciudad:</label>
       </div>
 
       <div className="input-field col s12">
-        <select name="pais" id="pais">
-            <option value="">--Seleccione un Pais</option>
+        <select name="pais" id="pais" value={pais} onChange={handleChange}>
+          <option value="">--Seleccione un Pais</option>
+          <option value="US">Estados Unidos</option>
+          <option value="MX">México</option>
+          <option value="AR">Argentina</option>
+          <option value="CO">Colombia</option>
+          <option value="CR">Costa Rica</option>
+          <option value="ES">España</option>
+          <option value="PE">Perú</option>
         </select>
         <label htmlFor="pais">Pais:</label>
       </div>
