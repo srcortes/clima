3466a58540c168fe10c34cc40f80662e451c8eee
= <=>
2020-09-22 09:40:36 -0500

    se realiza seguimiento a componente clima y error

---
+++ src/Components/Clima.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Clima = ({ resultado }) => {
+  const { name, main, coord } = resultado;
+
+  if(!name) return null;
+
+  const kelvin = 273.15;
+
+  return (
+    <div className="card-panel white col s12">
+      <div className="black-text">
+        <h2>el clime de {name} es :</h2>
+       <p className="temperatura">
+           {parseFloat(main.temp - kelvin, 10).toFixed(2)} 
+           <span>&#x2103;</span>
+        </p> 
+        <p> Temperatura Maxima:
+           {parseFloat(main.temp_max - kelvin, 10).toFixed(2)} 
+           <span>&#x2103;</span>
+        </p>
+        <p>Temperatura Minima:
+           {parseFloat(main.temp_min - kelvin, 10).toFixed(2)} 
+           <span>&#x2103;</span>
+        </p>
+        <p>Coordenadas: 
+           {coord.lon} |  {coord.lat}
+          
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Clima;
