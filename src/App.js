37b31e1107e2878085b2b05b459707a357aecd8f
= <=>
2020-09-22 09:39:55 -0500

    se crea componente error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Formulario from "./Components/Formulario";
+import Clima from "./Components/Clima";
+import Error from "./Components/Errror"
 
 function App() {
   //state del formulario
@@ -10,6 +12,8 @@ function App() {
   });
 
   const [consultar, guardarConsultar] = useState(false);
+  const [resultado, guardarResultado] = useState({});
+  const [error, guardarError] = useState(false);
 
   //se extrae informacion del state
   const { ciudad, pais } = busqueda;
@@ -17,24 +21,31 @@ function App() {
   //useEffect cuando cambien los valores ciudad y pais se actualiza lo que este dentro del useEffect
   useEffect(() => {
     const consultarApi = async () => {
-      if(consultar){
+      if (consultar) {
         //Se inicia construccion o invocacion servicio
-      const appId = "ad019f1cbc0512a31d256fc69ffe56d4";
-      const urlConsumo = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const appId = "ad019f1cbc0512a31d256fc69ffe56d4";
+        const urlConsumo = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
-      const respuesta = await fetch(urlConsumo);
-      const resultado = await respuesta.json();
-      console.log(resultado)
+        const respuesta = await fetch(urlConsumo);
+        const resultado = await respuesta.json();
+        guardarResultado(resultado);
+        guardarConsultar(false);
+        console.log(resultado)  
+        if (resultado.cod === "404") guardarError(true);
+        else guardarError(false);
       }
-
-     
     };
     consultarApi();
   }, [consultar]);
 
+  let componente;
+
+  if (error) componente = <Error mensaje =  "No is found result for the city and country consulted "/>;
+  else componente = <Clima resultado={resultado} />;
+
   return (
     <Fragment>
-      <Header titulo="Weather Clima App" />
+      <Header titulo="Weather App" />
 
       <div className="contenedor-form">
         <div className="container">
@@ -46,7 +57,7 @@ function App() {
                 guardarConsultar={guardarConsultar}
               />
             </div>
-            <div className="col m6 s12">2</div>
+            <div className="col m6 s12">{componente}</div>
           </div>
         </div>
       </div>
